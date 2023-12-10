e2b0ccabb65bd47633fefdc204190f1b7df305eb
Dmitriy Burenok <dmitriyburenok@gmail.com>
2023-12-09 18:14:58 -0800

    add gunicorn

--- start_api.py
+++ start_api.py
@@ -2,22 +2,17 @@ from flask import Flask, jsonify, request
 from pathlib import Path
 from shutil import rmtree
 import uuid
-
-import argparse
 import time
 import torch
 import torch.backends.cudnn
-
 from models.experimental import attempt_load
 from utils.datasets import LoadImages
 from utils.general import check_img_size, non_max_suppression, scale_coords
-from utils.torch_utils import time_synchronized
 
 
 # Initialize Flask app
 app = Flask(__name__)
 Path('./temp').mkdir(exist_ok=True)
-opt = None
 
 # Initialize YOLOv7
 device = 'cpu' # CPU is more cost-effective for this project
@@ -48,10 +43,10 @@ def detect(source):
 
         # Inference
         with torch.no_grad():   # Calculating gradients would cause a GPU memory leak
-            pred = model(img, augment=opt.augment)[0]
+            pred = model(img, augment=False)[0]
 
         # Apply NMS
-        pred = non_max_suppression(pred, opt.conf_thres, opt.iou_thres, classes=opt.classes, agnostic=opt.agnostic_nms)
+        pred = non_max_suppression(pred, 0.25, 0.45, classes=None, agnostic=False)
 
         # Process detections
         vehicle_count = 0
@@ -102,25 +97,4 @@ def count_vehicles():
 
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--weights', nargs='+', type=str, default='yolov7.pt', help='model.pt path(s)')
-    parser.add_argument('--source', type=str, default='inference/images', help='source')  # file/folder, 0 for webcam
-    parser.add_argument('--img-size', type=int, default=640, help='inference size (pixels)')
-    parser.add_argument('--conf-thres', type=float, default=0.25, help='object confidence threshold')
-    parser.add_argument('--iou-thres', type=float, default=0.45, help='IOU threshold for NMS')
-    parser.add_argument('--device', default='', help='cuda device, i.e. 0 or 0,1,2,3 or cpu')
-    parser.add_argument('--view-img', action='store_true', help='display results')
-    parser.add_argument('--save-txt', action='store_true', help='save results to *.txt')
-    parser.add_argument('--save-conf', action='store_true', help='save confidences in --save-txt labels')
-    parser.add_argument('--nosave', action='store_true', help='do not save images/videos')
-    parser.add_argument('--classes', nargs='+', type=int, help='filter by class: --class 0, or --class 0 2 3')
-    parser.add_argument('--agnostic-nms', action='store_true', help='class-agnostic NMS')
-    parser.add_argument('--augment', action='store_true', help='augmented inference')
-    parser.add_argument('--update', action='store_true', help='update all models')
-    parser.add_argument('--project', default='runs/detect', help='save results to project/name')
-    parser.add_argument('--name', default='exp', help='save results to project/name')
-    parser.add_argument('--exist-ok', action='store_true', help='existing project/name ok, do not increment')
-    parser.add_argument('--no-trace', action='store_true', help='don`t trace model')
-    opt = parser.parse_args()
-    print(opt)
     app.run(host='0.0.0.0', port=7777, debug=False)
